5cbaf7931b416afe232f45c319c38157bf48b069
Calvin <calvinchancan@gmail.com>
2023-03-02 18:12:53 -0500

    Add home page placeholder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+} from "react-router-dom";
+
 import './App.css';
-import { Button } from '@mui/material';
+import Home from "./pages/Home/Home";
+
+const Layout = () => {
+    return (
+        <div className="app">
+            <Outlet/>
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            }
+        ]
+    }
+
+])
+
+
 
 function App() {
-    const [count, setCount] = useState(0);
 
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React + + Typescript + MUI 5</h1>
-            <Button color="secondary">Secondary</Button>
-            <Button variant="contained" color="success">
-                Success
-            </Button>
-            <Button variant="outlined" color="error">
-                Error
-            </Button>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+        <div>
+            <RouterProvider router={router}/>
         </div>
     );
 }
