51155c82999862e7671bb35b33401eba18ee52cb
Calvin <calvinchancan@gmail.com>
2023-03-03 00:30:45 -0500

    Add API_URL to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {
+      GPA_API_URL: 'http://127.0.0.1:8000',
+    }
+  },
   plugins: [react()],
 })
