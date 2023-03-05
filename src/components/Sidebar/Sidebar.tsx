74e20c95021dd63e83b12e1fd299a790da31734e
Calvin <calvinchancan@gmail.com>
2023-03-05 16:38:49 -0500

    Add link to accounts and transactions page.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,20 @@
-import * as React from 'react';
+import React from 'react';
 import {List, ListItem, Divider} from '@mui/material';
 import './Sidebar.scss';
 import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
 
 export default function Sidebar() {
+    const navigate = useNavigate();
+
+    const handleTransactionsClick = () => {
+        navigate('/transactions?show=all');
+    }
+
+    const handleAccountsClick = () => {
+        navigate('/accounts');
+    }
+
     return (
         <div className="sidebar-container">
             <div className="sidebar">
@@ -11,10 +22,12 @@ export default function Sidebar() {
                 <Divider/>
                 <List>
                     <ListItem>
-                        <Button variant="outlined" className="sidebar-button">Accounts</Button>
+                        <Button variant="outlined" className="sidebar-button"
+                                onClick={handleAccountsClick}>Accounts</Button>
                     </ListItem>
                     <ListItem>
-                        <Button variant="contained" className="sidebar-button">Transactions</Button>
+                        <Button variant="contained" className="sidebar-button"
+                                onClick={handleTransactionsClick}>Transactions</Button>
                     </ListItem>
                 </List>
             </div>
