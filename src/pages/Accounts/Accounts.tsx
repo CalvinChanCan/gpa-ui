c6826e8cdff8a787bac7647803b69608612926eb
Calvin <calvinchancan@gmail.com>
2023-03-06 01:55:55 -0500

    Replace user id placeholder with user id in localstorage

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -3,14 +3,15 @@ import {makeRequest} from '../../utils/makeRequest';
 import {Account} from "../../types/account";
 import AccountCard from "../../components/AccountCard/AccountCard";
 import './Accounts.scss';
+import {getUserFromLocalStorage} from "../../utils/localStorageUtils";
 
 const Accounts = () => {
     const [accounts, setAccounts] = useState<Account[]>([]);
 
     useEffect(() => {
         const fetchAccounts = async () => {
-            const res = await makeRequest.get('/api/users/1/accounts');
-            console.log('res', res)
+            const user = getUserFromLocalStorage()
+            const res = await makeRequest.get(`/api/users/${user.id}/accounts/`);
             setAccounts(res.data as Account[]);
         };
 
